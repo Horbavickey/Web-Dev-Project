2bc17487e0535a0940f5a80e9dc4108a0d9835fe
Obanla <obanlajesupemi2021@gmail.com>
2024-04-09 04:37:35 +0700

    compiling my algorithms

--- Astar Algorithm/Draw_maze.js
+++ Astar Algorithm/Draw_maze.js
@@ -1,8 +1,12 @@
+//Maze generation algorithm modified from https://cantwell-tom.medium.com/prims-algorithm-as-a-maze-in-javascript-aec7415ad2cd
 function generatePrimMaze() {
     //making maze full of walls
     // console.log("Starting process of maze generation");
-   time = 0;
-    // to remove place holders
+
+
+    time = 0;
+
+    //removing place holders
     startx = -1;
     starty = -1;
     endx = -1;
@@ -140,60 +144,4 @@ function generatePrimMaze() {
     }, time);
 
     n = (2 * n) + overflow;
-}
-//traces path from end to start
-function tracePath(cellDetails, dest) {
-    var x = dest.x;
-    var y = dest.y;
-
-    while (!(cellDetails[x][y].parent_i == x && cellDetails[x][y].parent_j == y)) {
-        var temp_x = cellDetails[x][y].parent_i;
-        var temp_y = cellDetails[x][y].parent_j;
-        drawPath(x, y, temp_x, temp_y);
-        x = temp_x;
-        y = temp_y;
-    }
-
-    const button = document.getElementById("clearPath");
-    button.disabled = false;
-    l++;
-}
-
-function drawPath(x, y, temp_x, temp_y) {
-
-    l++;
-    timeoutPathID[l] = setTimeout(function () {
-
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 3;
-        ctx.beginPath();
-        ctx.moveTo(x * cellSide + cellSide / 2, y * cellSide + cellSide / 2);
-
-        //going down
-        if (y - temp_y > 0 && x - temp_x == 0) {
-            ctx.lineTo(temp_x * cellSide + cellSide / 2, temp_y * cellSide + cellSide / 2 - ctx.lineWidth / 2);
-        }
-
-        //going up
-        else if (y - temp_y < 0 && x - temp_x == 0) {
-            ctx.lineTo(temp_x * cellSide + cellSide / 2, temp_y * cellSide + cellSide / 2 + ctx.lineWidth / 2);
-        }
-
-        //going right
-        else if (x - temp_x < 0 && y - temp_y == 0) {
-            ctx.lineTo(temp_x * cellSide + cellSide / 2 + ctx.lineWidth / 2, temp_y * cellSide + cellSide / 2);
-        }
-
-        //going left
-        else if (x - temp_x > 0 && y - temp_y == 0) {
-            ctx.lineTo(temp_x * cellSide + cellSide / 2 - ctx.lineWidth / 2, temp_y * cellSide + cellSide / 2);
-        }
-
-        else {
-            ctx.lineTo(temp_x * cellSide + cellSide / 2, temp_y * cellSide + cellSide / 2);
-        }
-
-        ctx.stroke();
-
-    }, time);
 }
